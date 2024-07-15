ce48bb8ada0d707c9446d577feddb0af72d2a224
Muntasir2762 <muntasirpranto2762@gmail.com>
2024-07-15 19:36:22 +0600

    Homepage issue is fixed

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -9,18 +9,21 @@
 					<ul class="header__category-list">
 						@foreach ($allCategories as $category)
 						<li class="header__category-list-item item-has-submenu">
-							<a href="" class="header__category-list-item-link">
+							<a href="{{url('category-products/'.$category->slug)}}" class="header__category-list-item-link">
 								<img src="{{asset('backend/images/category/'.$category->image)}}" alt="category">
 								{{$category->name}}
 							</a>
 							<ul class="header__nav-item-category-submenu">
+								@foreach ($category->subCategories as $subCategory)
 								<li class="header__category-submenu-item">
-									<a href="sub-category-product.html" class="header__category-submenu-item-link">
-										Test Subcategory
+									<a href="{{url('sub-category-products/'.$subCategory->slug)}}" class="header__category-submenu-item-link">
+										{{$subCategory->name}}
 									</a>
 								</li>
+								@endforeach
 							</ul>
 						</li>
+						@endforeach
 					</ul>
 				</div>
 				<div class="home__slider-items-wrapper">
@@ -42,13 +45,15 @@
 				</h1>
 			</div>
 			<div class="categoris-items-wrapper owl-carousel">
-				<a href="#" class="categoris-item">
-					<img src="{{asset('frontend/assets/images/product.png')}}" alt="category" />
+				@foreach ($allCategories as $category)
+				<a href="{{url('category-products/'.$category->slug)}}" class="categoris-item">
+					<img src="{{asset('backend/images/category/'.$category->image)}}" alt="category" />
 					<h6 class="categoris-name">
-						Test Category
+						{{$category->name}}
 					</h6>
 					<span class="items-number">1 items</span>
 				</a>
+				@endforeach
 			</div>
 		</div>
 	</section>
@@ -143,7 +148,7 @@
 							<img src="{{asset('backend/images/product/'.$product->image)}}" alt="Product Image" />
 						</a>
 						<div class="product__item-add-cart-btn-outer">
-							<a href="{{url('/product/addtocart/'.$product->id)}}"  class="product__item-add-cart-btn-inner">
+							<a href="{{url('/product/addtocart/'.$product->id)}}" class="product__item-add-cart-btn-inner">
 								Add to Cart
 							</a>
 						</div>
@@ -191,7 +196,7 @@
 							<img src="{{asset('backend/images/product/'.$product->image)}}" alt="Product Image" />
 						</a>
 						<div class="product__item-add-cart-btn-outer">
-							<a href="{{url('/product/addtocart/'.$product->id)}}"  class="product__item-add-cart-btn-inner">
+							<a href="{{url('/product/details/'.$product->slug)}}" class="product__item-add-cart-btn-inner">
 								Add to Cart
 							</a>
 						</div>
